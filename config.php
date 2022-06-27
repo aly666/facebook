c329c0657f7b2a32d5d11cc85bfbefd6e4adf35f
Sutrisno <uchihaaliang99@gmail.com>
2022-06-27 20:17:19 +0700

    update

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "devopscilsy";
-$db_pass = "1234567890";
+$db_host = getenv("DB_HOST");
+$db_user = getenv("DB_USER");
+$db_pass = getenv("DB_PASS");
 $db_name = "dbsosmed";
 
-try {    
-    //create PDO connection 
-    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+
+try {
+//create PDO connection
+$db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 } catch(PDOException $e) {
-    //show error
-    die("Terjadi masalah: " . $e->getMessage());
+//show error
+die("Terjadi masalah: " . $e->getMessage());
 }
